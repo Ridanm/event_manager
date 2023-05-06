6eed73dfda137c2ddbfd05f7b4937f9d501bdb5a
Ghost <danyfox1.dm@gmail.com>
2023-05-06 12:07:43 -0300

    validate fixed phone

--- validate_phone.rb
+++ validate_phone.rb
@@ -5,6 +5,7 @@
 # 4- Si el número de teléfono tiene 11 dígitos y el primer número no es 1, entonces es un número incorrecto
 # 5- Si el número de teléfono tiene más de 11 dígitos, suponga que es un número incorrecto 
 require 'csv'
+require 'colorize'
 
 def validate_phone_numbers(phone)
   numbers = ''
@@ -16,19 +17,21 @@ def validate_phone_numbers(phone)
   end
   
   if numbers.size < 10 # 1
-    puts 'Incorrect phone number'
+    puts 'Wrong phone number'
   elsif numbers.size == 10 # 2
-    puts 'Correct number'
+    puts 'The phone number is correct'
     puts "(#{numbers[0..2]}) #{numbers[3..5]}-#{numbers[6..10]}"
   elsif numbers.size == 11 && numbers[0] == '1' # 3
-    puts 'Comienza con 1'
+    puts 'Starts with 1'
     puts "(#{numbers[1..3]}) #{numbers[4..6]}-#{numbers[7..11]}"
   elsif numbers.size == 11 && numbers[0] != '1' # 4
-    puts 'No comienza con 1 no es correcto'
+    puts 'Wrong phone number 11 digits does not start with 1'
   elsif numbers.size > 11 # 5 
-    puts 'El número no es correcto mas de 11 dígitos'
+    puts 'Wrong phone number more than 11 digits'
   end
 end
+
+
 
 def correct_phone(phone)
   numbers = '' 
@@ -36,12 +39,15 @@ def correct_phone(phone)
   phone.each_char { |num| numbers << num if num.between?('0', '9')}
 
   if numbers.size == 10
-    puts 'Correct number'
+    print 'Correct numbers: '.colorize(:green)
     puts "(#{numbers[0..2]}) #{numbers[3..5]}-#{numbers[6..10]}"
   elsif numbers.size == 11 && numbers[0] == '1'
+    print 'Correct numbers: '.colorize(:yellow)
     puts "(#{numbers[1..3]}) #{numbers[4..6]}-#{numbers[7..11]}"
   end
 end
+
+puts "\nThe Phone numbers that have been entered correctly\n".colorize(:green)
 
 contents = CSV.open(
   '../event_attendees.csv', 
@@ -50,6 +56,5 @@ contents = CSV.open(
 )
 
 contents.each do |row|
-  puts row[:homephone]
-  puts validate_phone_numbers(row[:homephone])
+  puts correct_phone(row[:homephone])
 end
